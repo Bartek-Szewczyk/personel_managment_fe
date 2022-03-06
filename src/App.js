1fa2627905b4177359648dff03cee5bdb5c05172
Bartek-Szewczyk <ba20001000@gmail.com>
2022-03-06 20:11:58 +0100

    added login page route

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import Navigation from "../src/components/navigartion/navigation";
 import Statistic from "../src/pages/statistic/statistic";
 import Staff from "../src/pages/staff/staff";
 import Error from "../src/pages/error/error";
+import Login from "../src/pages/login/login";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
         <Route path="personel_managment_fe" element={<Home />} />
         <Route path="statistic" element={<Statistic />} />
         <Route path="staff" element={<Staff />} />
+        <Route path="login" element={<Login />} />
         <Route path="*" element={<Error />} />
       </Route>
     </Routes>
