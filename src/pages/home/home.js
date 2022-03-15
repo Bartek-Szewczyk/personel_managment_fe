4cc0e29dd42bb22ce4018cf33b0909a2b3e7eec2
Bartek-Szewczyk <ba20001000@gmail.com>
2022-03-15 22:43:52 +0100

    added calendar style

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,61 @@
 import React from "react";
 import Layout from "../../components/layout/layout";
+import FullCalendar, { formatDate } from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { INITIAL_EVENTS, createEventId } from "./event-utils";
+
 import "./home.scss";
 
 function Home() {
+  const handleDateSelect = (selectInfo) => {
+    let title = prompt("Please enter a new title for your event");
+    let calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      });
+    }
+  };
+  const handleEventClick = (clickInfo) => {
+    if (
+      confirm(
+        `Are you sure you want to delete the event '${clickInfo.event.title}'`
+      )
+    ) {
+      clickInfo.event.remove();
+    }
+  };
   return (
     <div className="homeContainer">
-      <Layout title="Home" buttonText="Dodaj zmiane"></Layout>
+      <Layout title="Kalendarz">
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          initialView="dayGridMonth"
+          headerToolbar={{
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay",
+          }}
+          editable={true}
+          selectable={true}
+          selectMirror={true}
+          dayMaxEvents={true}
+          locale="pl"
+          firstDay={1}
+          height="90vh"
+          select={handleDateSelect}
+          eventClick={handleEventClick}
+        />
+      </Layout>
     </div>
   );
 }
