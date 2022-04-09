d80077182c68dc7a34678a465952aa86939de444
Bartek-Szewczyk <ba20001000@gmail.com>
2022-04-09 20:45:39 +0200

    added bar, line, pie charts

--- src/pages/statistic/statistic.js
+++ src/pages/statistic/statistic.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import Layout from "../../components/layout/layout";
 import Modal from "../../components/modal/modal";
+import LineChart from "../../components/charts/line";
+import PieChart from "../../components/charts/pie";
+import BarChart from "../../components/charts/bar";
 import "./statistic.scss";
 
 const Statistic = () => {
@@ -14,11 +17,19 @@ const Statistic = () => {
   return (
     <div className="statisticWrapper">
       <Modal show={modal} handleClose={handleClose}></Modal>
-      <Layout
-        title="Statystyki"
-        buttonText="Filtry"
-        buttonAction={handleOpen}
-      ></Layout>
+      <Layout title="Statystyki" buttonText="Filtry" buttonAction={handleOpen}>
+        <div className="statisticWrapper__chartWrapper">
+          <div className="statisticWrapper__barChart">
+            <BarChart />
+          </div>
+          <div className="statisticWrapper__doughnutChart">
+            <PieChart />
+          </div>
+        </div>
+        <div className="statisticWrapper__lineChart">
+          <LineChart />
+        </div>
+      </Layout>
     </div>
   );
 };
