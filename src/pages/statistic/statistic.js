375de74bba3aab870242d2cc59731e78a1f52512
Bartek-Szewczyk <ba20001000@gmail.com>
2022-04-10 11:39:31 +0200

    remove button form statistic

--- src/pages/statistic/statistic.js
+++ src/pages/statistic/statistic.js
@@ -1,23 +1,14 @@
 import React, { useState } from "react";
 import Layout from "../../components/layout/layout";
-import Modal from "../../components/modal/modal";
 import LineChart from "../../components/charts/line";
 import PieChart from "../../components/charts/pie";
 import BarChart from "../../components/charts/bar";
 import "./statistic.scss";
 
 const Statistic = () => {
-  const [modal, setModal] = useState(false);
-  const handleClose = () => {
-    setModal(false);
-  };
-  const handleOpen = () => {
-    setModal(true);
-  };
   return (
     <div className="statisticWrapper">
-      <Modal show={modal} handleClose={handleClose}></Modal>
-      <Layout title="Statystyki" buttonText="Filtry" buttonAction={handleOpen}>
+      <Layout title="Statystyki">
         <div className="statisticWrapper__chartWrapper">
           <div className="statisticWrapper__barChart">
             <BarChart />
