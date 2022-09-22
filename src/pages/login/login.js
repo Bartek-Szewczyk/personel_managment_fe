75df50393abb2101238c72aa7570452597fa3bf5
Bartek-Szewczyk <ba20001000@gmail.com>
2022-09-22 15:49:51 +0200

    corret login form

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./login.scss";
 import useAuth from "../../services/auth/hooks";
 
@@ -7,65 +7,24 @@ function Login() {
   const [fields, setFields] = useState({ email: "", password: "" });
   const [reset, setReset] = useState(false);
   const submit = (e) => {
+    e.preventDefault();
     onLogin(fields.email, fields.password);
   };
-  const isAdmin = roles?.indexOf("Admin") != -1;
   return (
-    <div className="loginWrapper">
-      <div className="loginWrapper__loginContainer">
-        {/* {!isAdmin && (
+    <form onSubmit={submit}>
+      <div className="loginWrapper">
+        <div className="loginWrapper__loginContainer">
+          {/* {!isAdmin && (
           <p className="loginWrapper__loginContainer__errorMessage">
             Nie masz dostępu do tej aplikacji
           </p>
         )} */}
-        {noAuth && (
-          <p className="loginWrapper__loginContainer__errorMessage">
-            Niepoprawny login lub hasło
-          </p>
-        )}
-        <h1 className="loginWrapper__loginContainer__title">Login</h1>
-        <div
-          name="loginForm"
-          className="loginWrapper__loginContainer__inputContainer"
-        >
-          <input
-            type="email"
-            name="email"
-            className="loginWrapper__loginContainer__inputContainer__mail"
-            placeholder="email"
-            required
-            onChange={(e) => {
-              setFields({ ...fields, email: e.target.value });
-            }}
-          ></input>
-          <input
-            type="password"
-            name="password"
-            className="loginWrapper__loginContainer__inputContainer__password"
-            placeholder="password"
-            autoComplete="on"
-            required
-            onChange={(e) => {
-              setFields({ ...fields, password: e.target.value });
-            }}
-          ></input>
-          {/* <Link to="/personel_managment_fe" style={{ width: "100%" }}> */}
-          <input
-            style={{ width: "100%" }}
-            type="submit"
-            className="loginWrapper__loginContainer__inputContainer__button"
-            value="Login"
-            onClick={submit}
-          ></input>
-          {/* </Link> */}
-        </div>
-        <p
-          className="loginWrapper__loginContainer__link"
-          onClick={() => setReset(reset ? false : true)}
-        >
-          Reset hasła?
-        </p>
-        {reset && (
+          {noAuth && (
+            <p className="loginWrapper__loginContainer__errorMessage">
+              Niepoprawny login lub hasło
+            </p>
+          )}
+          <h1 className="loginWrapper__loginContainer__title">Login</h1>
           <div
             name="loginForm"
             className="loginWrapper__loginContainer__inputContainer"
@@ -81,14 +40,55 @@ function Login() {
               }}
             ></input>
             <input
+              type="password"
+              name="password"
+              className="loginWrapper__loginContainer__inputContainer__password"
+              placeholder="Hasło"
+              autoComplete="on"
+              required
+              onChange={(e) => {
+                setFields({ ...fields, password: e.target.value });
+              }}
+            ></input>
+            <button
+              style={{ width: "100%" }}
               type="submit"
               className="loginWrapper__loginContainer__inputContainer__button"
-              value="Wyślij"
-            ></input>
+            >
+              Login
+            </button>
           </div>
-        )}
+          <p
+            className="loginWrapper__loginContainer__link"
+            onClick={() => setReset(reset ? false : true)}
+          >
+            Reset hasła?
+          </p>
+          {reset && (
+            <div
+              name="loginForm"
+              className="loginWrapper__loginContainer__inputContainer"
+            >
+              <input
+                type="email"
+                name="email"
+                className="loginWrapper__loginContainer__inputContainer__mail"
+                placeholder="email"
+                required
+                onChange={(e) => {
+                  setFields({ ...fields, email: e.target.value });
+                }}
+              ></input>
+              <input
+                type="submit"
+                className="loginWrapper__loginContainer__inputContainer__button"
+                value="Wyślij"
+              ></input>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </form>
   );
 }
 
